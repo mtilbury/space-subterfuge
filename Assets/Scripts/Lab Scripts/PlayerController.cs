00aa8c7157f81fb390dd8ebd4cf6d2c521688bab
dcsohn <dcsohn@umich.edu>
2019-10-25 17:40:07 -0400

    First Implementation
    Created a quick implementation of fog of war using Unity's lighting system.  Currently requires all other lighting to be removed as well as the skybox.  Folders for organization created.  Put scripts that are meant for quick testing in lab scenes in the "Lab Scripts" folder.

---
+++ Assets/Scripts/Lab Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+
+    private Rigidbody rb;
+    Vector3 movement;
+    private bool grounded = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(transform.position.y < -10f)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
+            //rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+            Debug.Log("Reset");
+            grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+            grounded = false;
+    }
+}
