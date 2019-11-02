c458c9c1999353061bafdbbebfa84d7787964f92
dcsohn <dcsohn@umich.edu>
2019-11-02 17:06:30 -0400

    Created Ping

---
+++ Assets/TimedDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour
+{
+
+	public float destroyTime = 3.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		Destroy(gameObject, destroyTime);
+    }
+
+}
