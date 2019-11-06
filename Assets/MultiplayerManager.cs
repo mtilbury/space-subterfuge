9f1738349d06d57a242824bca5d2608387f8d9fa
Michael Tilbury <mtilbury@umich.edu>
2019-11-06 16:00:30 -0500

    Controller mapping to players is persistent across scene loads

---
+++ Assets/MultiplayerManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MultiplayerManager : MonoBehaviour
+{
+    public GameObject GamepadManagerPrefab;
+
+    // Players
+    public GameObject defender;
+    public GameObject attacker1;
+    public GameObject attacker2;
+    public GameObject attacker3;
+
+    // Direct access to players' movement components
+    private PlayerMovement defender_mov;
+    private PlayerMovement attacker1_mov;
+    private PlayerMovement attacker2_mov;
+    private PlayerMovement attacker3_mov;
+
+    private int numControllers = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate a GamepadManager
+        Instantiate(GamepadManagerPrefab);
+
+        defender_mov = defender.GetComponent<PlayerMovement>();
+        attacker1_mov = attacker1.GetComponent<PlayerMovement>();
+        attacker2_mov = attacker2.GetComponent<PlayerMovement>();
+        attacker3_mov = attacker3.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(numControllers < 4)
+        {
+            // Try to get defender gamepad
+            Gamepad defender_gp = GamepadManager.getPlayerController(1);
+            if (defender_gp != null)
+            {
+                numControllers++;
+                defender_mov.controller = defender_gp;
+            }
+
+            // Try to get attacker1 gamepad
+            Gamepad attacker1_gp = GamepadManager.getPlayerController(2);
+            if(attacker1_gp != null)
+            {
+                numControllers++;
+                attacker1_mov.controller = attacker1_gp;
+            }
+
+            // Try to get attacker1 gamepad
+            Gamepad attacker2_gp = GamepadManager.getPlayerController(3);
+            if (attacker1_gp != null)
+            {
+                numControllers++;
+                attacker2_mov.controller = attacker2_gp;
+            }
+
+            // Try to get attacker1 gamepad
+            Gamepad attacker3_gp = GamepadManager.getPlayerController(4);
+            if (attacker1_gp != null)
+            {
+                numControllers++;
+                attacker3_mov.controller = attacker3_gp;
+            }
+        }
+    }
+}
--- Assets/Scripts/GamepadManager.cs
+++ Assets/Scripts/GamepadManager.cs
@@ -5,55 +5,88 @@ using UnityEngine.InputSystem;
 
 public class GamepadManager : MonoBehaviour
 {
-    public GameObject Defender;
-    public GameObject Attacker1;
-    public GameObject Attacker2;
-    public GameObject Attacker3;
+    // Singleton!
+    public static GamepadManager instance;
 
-    public HashSet<Gamepad> UsedGamepads;
+    // Maintain set of tracked gamepads
+    private static HashSet<Gamepad> UsedGamepads;
+
+    // Maintain map of player => controller
+    private static Dictionary<int, Gamepad> ControllerMap;
+
+    // Are all gamepads populated?
+    private bool allGamepadsPopulated = false;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         UsedGamepads = new HashSet<Gamepad>();
+        ControllerMap = new Dictionary<int, Gamepad>
+        {
+            { 1, null },
+            { 2, null },
+            { 3, null },
+            { 4, null }
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Gamepad gamepad in Gamepad.all)
+        if (!allGamepadsPopulated)
         {
-            if (UsedGamepads.Contains(gamepad))
-                continue;
+            foreach (Gamepad gamepad in Gamepad.all)
+            {
+                if (UsedGamepads.Contains(gamepad))
+                    continue;
 
-            bool a_pressed_this_frame = gamepad.aButton.wasPressedThisFrame;
+                bool a_pressed_this_frame = gamepad.aButton.wasPressedThisFrame;
 
-            if (Defender.GetComponent<PlayerMovement>().controller == null && a_pressed_this_frame)
-            {
-                Defender.GetComponent<PlayerMovement>().controller = gamepad;
-                UsedGamepads.Add(gamepad);
-                Debug.Log("Defender is online");
-            }
-            else if (Attacker1.GetComponent<PlayerMovement>().controller == null && a_pressed_this_frame)
-            {
-                Attacker1.GetComponent<PlayerMovement>().controller = gamepad;
-                UsedGamepads.Add(gamepad);
-                Debug.Log("Attacker 1 is online");
-            }
-            else if (Attacker2.GetComponent<PlayerMovement>().controller == null && a_pressed_this_frame)
-            {
-                Attacker2.GetComponent<PlayerMovement>().controller = gamepad;
-                UsedGamepads.Add(gamepad);
-                Debug.Log("Attacker 2 is online");
-            }
-            else if (Attacker3.GetComponent<PlayerMovement>().controller == null && a_pressed_this_frame)
-            {
-                Attacker3.GetComponent<PlayerMovement>().controller = gamepad;
-                UsedGamepads.Add(gamepad);
-                Debug.Log("Attacker 3 is online");
+                if (ControllerMap[1] == null && a_pressed_this_frame)
+                {
+                    ControllerMap[1] = gamepad;
+                    UsedGamepads.Add(gamepad);
+                    Debug.Log("Defender is online");
+                }
+                else if (ControllerMap[2] == null && a_pressed_this_frame)
+                {
+                    ControllerMap[2] = gamepad;
+                    UsedGamepads.Add(gamepad);
+                    Debug.Log("Attacker 2 is online");
+                }
+                else if (ControllerMap[3] == null && a_pressed_this_frame)
+                {
+                    ControllerMap[3] = gamepad;
+                    UsedGamepads.Add(gamepad);
+                    Debug.Log("Attacker 3 is online");
+                }
+                else if (ControllerMap[4] == null && a_pressed_this_frame)
+                {
+                    ControllerMap[4] = gamepad;
+                    UsedGamepads.Add(gamepad);
+                    Debug.Log("Attacker 4 is online");
+                    allGamepadsPopulated = true;
+                }
             }
         }
+    }
 
-        
+    public static Gamepad getPlayerController(int playerID)
+    {
+        return ControllerMap[playerID];
     }
 }
