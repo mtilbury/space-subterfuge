5f224183cceb7bd32ede4475ddbf268a718ae5df
webbling <webbling@umich.edu>
2019-11-10 13:36:32 -0500

    Added Jail, fixed Defender Win Condition

--- Assets/JailManagement.cs
+++ Assets/JailManagement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class JailManagement : MonoBehaviour
 {
+    public Queue<GameObject> jailedAttackers = new Queue<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
