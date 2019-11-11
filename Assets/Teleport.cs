e220ba95f6c5d8e5938c6391dbc5d45c3b5c5b51
webbling <webbling@umich.edu>
2019-11-10 22:03:36 -0500

    Teleporter Done

---
+++ Assets/Teleport.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject begin;
+    public GameObject end;
+    public bool canTeleport = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Defender"))
+        {
+            if (canTeleport && end.GetComponent<Teleport>().canTeleport)
+            {
+                canTeleport = false;
+                other.transform.position = end.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Defender"))
+        {
+            if (!canTeleport)
+            {
+
+            }
+            else
+            {
+                canTeleport = true;
+                end.GetComponent<Teleport>().canTeleport = true;
+            }
+        }
+    }
+}
