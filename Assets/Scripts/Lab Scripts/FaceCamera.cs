a6de0a60c21b9fed0b835371600c7af400c2ac4d
dcsohn <dcsohn@umich.edu>
2019-11-10 22:34:25 -0500

    Push

---
+++ Assets/Scripts/Lab Scripts/FaceCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera targetCamera;
+
+    private void LateUpdate()
+    {
+        this.transform.up =  - targetCamera.transform.forward;
+    }
+}
