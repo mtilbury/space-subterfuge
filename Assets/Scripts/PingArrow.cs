8e7154cf6671444d9cf12ff51c52b011749ba04f
dcsohn <dcsohn@umich.edu>
2019-11-16 20:56:55 -0500

    New Ping System Almost Done

--- Assets/Scripts/PingArrow.cs
+++ Assets/Scripts/PingArrow.cs
@@ -5,22 +5,31 @@ using UnityEngine.UI;
 
 public class PingArrow : MonoBehaviour
 {
-    public enum SetScreen { Attacker1 = 3, Attacker2 = 4, Attacker3  = 5, Defender = 6};
+    public enum SetScreen { Attacker1, Attacker2, Attacker3, Defender};
 
-    public SetScreen targetScreen; 
-    public Camera targetCamera;
-    public Canvas targetCanvas;
+    public SetScreen targetScreen = 0;
+    [SerializeField]
+    private Camera targetCamera;
+    [SerializeField]
+    private Canvas targetCanvas;
+
+    [Space]
 
     private RectTransform m_icon;
     private Image m_iconImage;
 
     public Sprite onScreenSprite;
     public Sprite offScreenSprite;
+    public Color spriteColor = Color.white;
     [Space]
-    [Range(0, 1000)]
-    public float m_edgeBuffer;
-    [Range(0, 100)]
-    public float m_innerEdgeBuffer;
+    [Range(0, 600)]
+    public float leftLimit;
+    [Range(0, 600)]
+    public float rightLimit;
+    [Range(0, 600)]
+    public float upLimit;
+    [Range(0, 600)]
+    public float downLimit;
 
     [Range(0, 1)]
     public float disappearRange;
@@ -32,26 +41,12 @@ public class PingArrow : MonoBehaviour
 
     private bool isOnScreen;
 
-    private int screenWidth;
-    private int screenHeight;
 
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        Camera[] cameras;
-        cameras = FindObjectsOfType<Camera>();
-        targetCamera = cameras[(int)targetScreen];
-        */
-
-        screenWidth = Screen.width;
-        screenHeight = Screen.height;
-
-        if(targetScreen != SetScreen.Defender)
-        {
-            screenWidth /= 4;
-            screenHeight /= 4;
-        }
+        targetCamera = PingManager.Instance.GetCamera((int)targetScreen);
+        targetCanvas = PingManager.Instance.GetCanvas((int)targetScreen);
 
         InstainateTargetIcon();
     }
@@ -73,12 +68,14 @@ public class PingArrow : MonoBehaviour
         m_icon.name = name + ": OTI icon";
         m_iconImage = m_icon.gameObject.AddComponent<Image>();
         m_iconImage.sprite = onScreenSprite;
+        m_iconImage.color = spriteColor;
     }
 
     private void UpdateTargetPosition()
     {
         Vector3 newPos = transform.position;
         newPos = targetCamera.WorldToViewportPoint(newPos);
+        Debug.Log(newPos);
         //Simple check if the target object is out of the screen or inside
      
 
@@ -97,22 +94,33 @@ public class PingArrow : MonoBehaviour
                 m_iconImage.enabled = true;
         }
 
+        
         if (newPos.z < 0)
         {
+            /*
             newPos.x = 1f - newPos.x;
             newPos.y = 1f - newPos.y;
             newPos.z = 0;
             newPos = Vector3Maxamize(newPos);
+            */
+
+            newPos.x = 1f - newPos.x;
+            newPos.y *= -1;
+            newPos.z *= -1;
         }
+        
 
         newPos = targetCamera.ViewportToScreenPoint(newPos);
-        newPos.x = Mathf.Clamp(newPos.x, m_edgeBuffer, screenWidth- m_edgeBuffer);
-        newPos.y = Mathf.Clamp(newPos.y, m_edgeBuffer, screenHeight - m_edgeBuffer);
+        newPos.x = Mathf.Clamp(newPos.x, leftLimit, Screen.width - rightLimit);
+        newPos.y = Mathf.Clamp(newPos.y, downLimit, Screen.height - upLimit);
+        newPos.z = 0;
+        //m_icon.anchoredPosition = newPos;
+        //Debug.Log(m_icon.name + newPos);
         m_icon.transform.position = newPos;
         //Operations if the object is out of the screen
         if (!isOnScreen)
         {
-            Debug.Log("Is not on screen");
+            //Debug.Log("Is not on screen");
             //Show the target off screen icon
             m_iconImage.sprite = offScreenSprite;
             m_icon.localScale = m_targetOffScreenScale;
@@ -128,7 +136,7 @@ public class PingArrow : MonoBehaviour
         }
         else
         {
-            Debug.Log("Is on screen");
+            //Debug.Log("Is on screen");
             //Reset rotation to zero and swap the sprite to the "on screen" one
             m_icon.transform.eulerAngles = new Vector3(0, 0, 0);
             m_iconImage.sprite = onScreenSprite;
@@ -162,5 +170,10 @@ public class PingArrow : MonoBehaviour
         Debug.DrawLine(targetCamera.transform.position, forwardPlaneCenter, Color.blue);
         Debug.DrawLine(forwardPlaneCenter, forwardPlaneCenter + cameraUp, Color.green);
         Debug.DrawLine(forwardPlaneCenter, forwardPlaneCenter + cameraRight, Color.red);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(m_icon.gameObject);
     }
 }
