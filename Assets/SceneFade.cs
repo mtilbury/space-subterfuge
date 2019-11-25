b20a6b72f532708812feeba17284af91b355b89d
dcsohn <dcsohn@umich.edu>
2019-11-25 12:53:41 -0500

    Merging with Master

---
+++ Assets/SceneFade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFade : MonoBehaviour
+{
+    public Animator animator1;
+    public Animator animator2;
+
+    private int levelToLoad;
+
+    public void FadeToLevel(int levelIndex)
+    {
+        levelToLoad = levelIndex;
+        animator1.SetTrigger("FadeOut");
+        animator2.SetTrigger("FadeOut");
+
+        StartCoroutine(OnFadeComplete());
+    }
+
+    private IEnumerator OnFadeComplete()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
