47cc3c2d6f30c7f230f14d17feb69361832f13c6
Michael Tilbury <mtilbury@umich.edu>
2019-12-04 17:12:28 -0500

    Work on title screen

---
+++ Assets/ContinuousHop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ContinuousHop : MonoBehaviour
+{
+    public GameObject player;
+    public float hopForce = 2.0f;
+    public float frequency = 8.0f;
+    public float offset = 0.5f;
+    Vector3 movementInput;
+
+    public Transform cam;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 oldPos = transform.position;
+        transform.position = (Vector3.up * (Mathf.Abs(Mathf.Sin(Time.time * frequency)) + offset) * hopForce);
+
+        transform.position = new Vector3(oldPos.x, transform.position.y, oldPos.z);
+
+    }
+
+}
---
+++ Assets/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.position, Vector3.up, 30 * Time.deltaTime);
+        transform.LookAt(target);
+    }
+}
