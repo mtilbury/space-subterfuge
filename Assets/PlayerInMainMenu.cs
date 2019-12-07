75c91fd60505fcc7bfdfc41b8faff0b316897c51
Michael Tilbury <mtilbury@umich.edu>
2019-12-07 17:21:10 -0500

    Work on title screen/ready up

---
+++ Assets/PlayerInMainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PlayerInMainMenu : MonoBehaviour, HasController
+{
+    public Gamepad controller { get; set; }
+
+    public Image button;
+
+    public Color selectedColor;
+    public Color defaultColor;
+
+    private bool joined = false;
+
+    public TitleScreenManager titleManager;
+    public int playerID;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (button.IsActive() && controller != null && controller.aButton.isPressed)
+        {
+            // Change text color
+            button.color = selectedColor;
+
+            // Register readyup success
+            joined = true;
+            titleManager.RegisterReadyUp(playerID);
+        }
+        else if (joined)
+        {
+            button.color = defaultColor;
+        }
+    }
+}
---
+++ Assets/TitleScreenManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public GameObject StartButtonPrompt;
+
+    public GameObject ReadyUpPanel;
+
+    public GameObject defender;
+    public GameObject attacker1;
+    public GameObject attacker2;
+    public GameObject attacker3;
+    private List<HasController> player_controllers;
+
+    public int numPlayers;
+    public SceneFade fader;
+    private HashSet<int> readyPlayers;
+
+    private bool showingReadyUp = false;
+
+    private void Start()
+    {
+        player_controllers = new List<HasController>
+        {
+            defender.GetComponent<HasController>(),
+            attacker1.GetComponent<HasController>(),
+            attacker2.GetComponent<HasController>(),
+            attacker3.GetComponent<HasController>()
+        };
+
+        readyPlayers = new HashSet<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!showingReadyUp)
+        {
+            // If START is pressed on any controller, show ready up screen
+            foreach (HasController controller in player_controllers)
+            {
+                if (controller.controller != null && controller.controller.startButton.wasPressedThisFrame)
+                {
+                    showingReadyUp = true;
+                    StartButtonPrompt.SetActive(false);
+                    ReadyUpPanel.SetActive(true);
+                }
+            }
+        }
+
+        if (readyPlayers.Count == numPlayers)
+        {
+            fader.FadeToLevel(2);
+        }
+    }
+
+    public void RegisterReadyUp(int playerID)
+    {
+        readyPlayers.Add(playerID);
+    }
+}
