d900dcb8b5139183db91efc684a0c7a41517e148
Michael Tilbury <mtilbury@umich.edu>
2019-12-07 21:34:04 -0500

    Add end game options

---
+++ Assets/EndScreenManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreenManager : MonoBehaviour
+{
+    //public GameObject StartButtonPrompt;
+    //public GameObject StartButtonPrompt2;
+
+    public GameObject ReadyUpPanel;
+    public GameObject ReadyUpPanel2;
+
+    public GameObject defender;
+    public GameObject attacker1;
+    public GameObject attacker2;
+    public GameObject attacker3;
+    private List<HasController> player_controllers;
+
+    public int numPlayers;
+    public SceneFade fader;
+
+    private HashSet<int> readyPlayers;
+    private HashSet<int> menuPlayers;
+
+    private bool showingReadyUp = false;
+
+    public float secondsBeforeReadyUp = 2.0f;
+
+    private void Start()
+    {
+        //player_controllers = new List<HasController>
+        //{
+        //    defender.GetComponent<HasController>(),
+        //    attacker1.GetComponent<HasController>(),
+        //    attacker2.GetComponent<HasController>(),
+        //    attacker3.GetComponent<HasController>()
+        //};
+
+        StartCoroutine(ShowReadyUpInSeconds());
+
+        readyPlayers = new HashSet<int>();
+        menuPlayers = new HashSet<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!showingReadyUp)
+        //{
+        //    // If START is pressed on any controller, show ready up screen
+        //    foreach (HasController controller in player_controllers)
+        //    {
+        //        if (controller.controller != null && controller.controller.startButton.wasPressedThisFrame)
+        //        {
+        //            showingReadyUp = true;
+        //            StartButtonPrompt.SetActive(false);
+        //            StartButtonPrompt2.SetActive(false);
+        //            ReadyUpPanel.SetActive(true);
+        //            ReadyUpPanel2.SetActive(true);
+        //        }
+        //    }
+        //}
+
+        if (readyPlayers.Count == numPlayers)
+        {
+            fader.FadeToLevel(3);
+        }
+        else if (menuPlayers.Count == numPlayers)
+        {
+            fader.FadeToLevel(1);
+        }
+    }
+
+    public void RegisterReadyUp(int playerID, bool aButton)
+    {
+        if (aButton)
+        {
+            readyPlayers.Add(playerID);
+        }
+        else
+        {
+            menuPlayers.Add(playerID);
+        }
+        
+    }
+
+    public void UnregisterReadyUp(int playerID, bool aButton)
+    {
+        if (aButton)
+        {
+            if(readyPlayers.Contains(playerID))
+                readyPlayers.Remove(playerID);
+        }
+        else
+        {
+            if (menuPlayers.Contains(playerID))
+                menuPlayers.Remove(playerID);
+        }
+    }
+
+    private IEnumerator ShowReadyUpInSeconds()
+    {
+        yield return new WaitForSeconds(secondsBeforeReadyUp);
+        ReadyUpPanel.SetActive(true);
+        ReadyUpPanel2.SetActive(true);
+    }
+}
---
+++ Assets/PlayerInEndScene.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerInEndScene : MonoBehaviour, HasController
+{
+    public Gamepad controller { get; set; }
+
+    public Image button;
+    public Image button2;
+
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI text2;
+
+    public Color selectedColorA;
+    public Color defaultColorA;
+    public Color selectedColorB;
+    public Color defaultColorB;
+
+    private bool joined = false;
+    private bool aButton = false;
+
+    public EndScreenManager endManager;
+    public int playerID;
+
+    private Vector3 defaultScale = Vector3.one;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (button.IsActive() && controller != null && controller.aButton.isPressed)
+        {
+            // Change text color
+            button.color = selectedColorA;
+            button.transform.localScale = defaultScale * 1.2f;
+            text.text = "A";
+            button2.color = selectedColorA;
+            button2.transform.localScale = defaultScale * 1.2f;
+            text2.text = "A";
+
+            // Register readyup success
+            joined = true;
+            aButton = true;
+            endManager.UnregisterReadyUp(playerID, false);
+            endManager.RegisterReadyUp(playerID, true);
+        }
+        else if(button.IsActive() && controller != null && controller.bButton.isPressed)
+        {
+            // Change text color
+            button.color = selectedColorB;
+            text.text = "B";
+            button.transform.localScale = defaultScale * 1.2f;
+            button2.color = selectedColorB;
+            text2.text = "B";
+            button2.transform.localScale = defaultScale * 1.2f;
+
+            // Register readyup success
+            joined = true;
+            aButton = false;
+            endManager.UnregisterReadyUp(playerID, true);
+            endManager.RegisterReadyUp(playerID, false);
+        }
+        else if (joined && aButton)
+        {
+            button.color = defaultColorA;
+            button.transform.localScale = defaultScale;
+            button2.color = defaultColorA;
+            button2.transform.localScale = defaultScale;
+        }
+        else if (joined)
+        {
+            button.color = defaultColorB;
+            button.transform.localScale = defaultScale;
+            button2.color = defaultColorB;
+            button2.transform.localScale = defaultScale;
+        }
+    }
+}
